4a3684a042dd26247657992a0f8a4f474527ac8e
Raven Timmer <timmerraven@gmail.com>
2022-12-02 13:47:52 +0100

    smol

--- day2/day2.py
+++ day2/day2.py
@@ -1,16 +1,8 @@
 #!/usr/bin/python3
-import os
-
-my_absolute_path = r'/home/raven/Documents/UVA/Advent_of_code/day2/input.txt'
-lines = open(my_absolute_path, 'r').readlines()
-
-total_points = 0
-played_hand = [2, 3, 1, 2, 3, 1, 2]
+lines = open(r'/home/raven/Documents/UVA/Advent_of_code/day2/input.txt', 'r').readlines()
+total_points = 0; played_hand = [2, 3, 1, 2, 3, 1, 2]
 
 for line in lines:
     x = line.split()
-
-    total_points += (ord(x[1]) - 88) * 3
-    total_points += played_hand[(4 + (ord(x[0]) - 65)) - ((ord(x[1]) - 88) * 2)]
-
+    total_points += ((ord(x[1]) - 88) * 3) + played_hand[(4 + (ord(x[0]) - 65)) - ((ord(x[1]) - 88) * 2)]
 print(total_points)
